4437d34b772b5924e371ce72bf53fd7c41388a84
liletian <liletian.us@gmail.com>
2019-11-13 10:51:25 -0800

    Add files via upload

---
+++ max_min.java
@@ -0,0 +1,20 @@
+class test{
+    public static void main(String[] args)
+    {
+	Character ch = new Character('a');
+	
+	System.out.println(ch.getClass());
+	System.out.println("a".getClass());
+	System.out.println(Long.MAX_VALUE);
+	System.out.println(Long.MIN_VALUE);
+	System.out.println(Integer.MIN_VALUE);
+	System.out.println(Integer.MIN_VALUE);
+	System.out.println(Float.MAX_VALUE);
+	System.out.println(Float.MIN_VALUE);
+	System.out.println(Double.MIN_VALUE);
+	System.out.println(Double.MAX_VALUE);
+	//	System.out.println(Character.MIN_VALUE);
+	//	System.out.println(Character.MAX_VALUE);
+	//	System.out.println('a'.getClass());
+    }
+}
